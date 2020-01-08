813f79254ac14a9b0e14d4d513441e253066aead
Douglas Mendes <mendes-barreto@live.com>
2020-01-07 23:16:05 -0500

    Add marco TEXT to cpp

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,12 +1,21 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
-void UBullCowCartridge::BeginPlay() // When the game starts
+// This will start when the game starts
+void UBullCowCartridge::BeginPlay()
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Well come to Bull Cows!"));
+    PrintLine(TEXT("Each wrong answer we will blow one cow!"));
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+// This method will trigger every time when player types enter
+void UBullCowCartridge::OnInput(const FString& Input)
 {
+    ClearScreen();
+
+    // Text MACRO will convert all strings in the right encoding
+    FString hiddenWord  = TEXT("Cow");
+
 
 }
