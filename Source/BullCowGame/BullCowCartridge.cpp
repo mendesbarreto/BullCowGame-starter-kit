405019d2c0d7a2ff16aa3829133b12f9300398c7
Douglas Mendes <mendes-barreto@live.com>
2020-02-20 00:19:19 -0500

    Add new conditions for my game loop

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,17 +5,57 @@
 void UBullCowCartridge::BeginPlay()
 {
     Super::BeginPlay();
+    this->SetupGame();
+
+    PrintLine(TEXT("The HiddenWord is: %s"), *this->m_hiddenWord);
+
     PrintLine(TEXT("Well come to Bull Cows!"));
+    PrintLine(TEXT("Guess the %i letter word!"), this->m_hiddenWord.Len());
     PrintLine(TEXT("Each wrong answer we will blow one cow!"));
+
+    PrintLine(TEXT("Type you guess and press ENTER"));
 }
 
 // This method will trigger every time when player types enter
 void UBullCowCartridge::OnInput(const FString& Input)
 {
-    ClearScreen();
+    if (this->m_isGameOver) {
+        this->ClearScreen();
+        this->SetupGame();
+    }else {
+        if(Input.ToLower() == m_hiddenWord.ToLower()) {
+            PrintLine(TEXT("I did it"));
+            this->EndGame();
+        } else {
+            PrintLine(TEXT("BOOM You are wrong!"));
+            auto inputLen = Input.Len();
+            auto hiddenWorld = this->m_hiddenWord.Len();
 
-    // Text MACRO will convert all strings in the right encoding
-    FString hiddenWord  = TEXT("Cow");
+            if(inputLen > hiddenWorld) {
+                PrintLine(TEXT("The hidden world has less chars"));
+            } else if (inputLen < hiddenWorld) {
+                PrintLine(TEXT("The hidden world has more chars"));
+            }
 
+            --this->m_lives;
 
+            PrintLine(TEXT("The number of lines you have is %i "), this->m_lives);
+
+            if (this->m_lives <= 0) {
+                this->EndGame();
+            }
+        }
+    }
+
+}
+
+void UBullCowCartridge::SetupGame() {
+    this->m_hiddenWord = TEXT("Cow");
+    this->m_lives = 4;
+    this->m_isGameOver = false;
+}
+
+void UBullCowCartridge::EndGame() {
+    this->m_isGameOver = true;
+    PrintLine(TEXT("Press enter to play again."));
 }
